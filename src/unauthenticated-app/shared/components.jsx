8884c2dc9266125ea4ee63fef46535ac524b5a49
moshekatz <moshek777@gmail.com>
2021-02-07 23:22:28 +0200

    changed the height from min to regular

--- src/unauthenticated-app/shared/components.jsx
+++ src/unauthenticated-app/shared/components.jsx
@@ -1,6 +1,6 @@
 export function UnauthenticatedAppWrapper({ children }) {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center space-y-6 bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 bg-gradient-to-br from-blue-200 via-gray-50 to-yellow-300">
+    <div className="h-screen flex flex-col items-center justify-center space-y-6 bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 bg-gradient-to-br from-blue-200 via-gray-50 to-yellow-300">
       <div className="fixed top-14 sm:top-1/4 max-w-md w-full space-y-4">
         <UnauthenticatedAppHeader />
         {children}
