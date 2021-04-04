2b54eb96a65a61e53ba6faf10afdd800bf45def5
moshekatz <moshek777@gmail.com>
2021-04-04 11:33:25 +0300

    fixed the cursor state when disabled button

--- src/authenticated-app/transaction-slide-over.jsx
+++ src/authenticated-app/transaction-slide-over.jsx
@@ -486,7 +486,7 @@ function TransactionSlideOverDetails({
                           type="submit"
                           className={`ml-4 ${
                             requiredFieldsInvalid
-                              ? "opacity-50"
+                              ? "opacity-50 cursor-not-allowed"
                               : "hover:bg-blue-700"
                           } inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500`}
                           disabled={requiredFieldsInvalid}
